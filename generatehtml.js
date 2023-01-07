d2ffd25391998a955218a07632395320ef3f47b5
Fleaux <hudsonfloyd@yahoo.com>
2023-01-07 15:45:31 -0600

    applied html template and modified the teammembermarkup

--- generatehtml.js
+++ generatehtml.js
@@ -1,9 +1,14 @@
+const Engineer = require("./lib/Engineer");
+
 function generateHtml(team = []) {
     team.forEach(member => console.log(member));
     const manager = team.find(member => member.role === 'Manager');
+    const engineer = team.find(member => member.role === 'Engineer');
+    const intern = team.find(member => member.role === 'Intern');
     const teamMemberMarkup = team
         .filter(member => member.role !== 'Manager')
-        .map(member => `<p>Team Member:${member.role} ${member.name}</p>`)
+        .map(member => `<h5>${member.name}</h5>
+        <h5>${member.role}</h5>`)
         .join(''); 
     return `
 <!DOCTYPE html>
@@ -12,7 +17,7 @@ function generateHtml(team = []) {
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Mock HTML</title>
+    <title>Team Member Profiles</title>
     <link rel="stylesheet" href="dist/styles.css" />
   </head>
   <body>
@@ -32,6 +37,51 @@ function generateHtml(team = []) {
           <h6>Office number:${manager.officeNumber}</h6>
         </div>
       </div>
+      <div class="card">
+        <div class="cardheader">
+        <h4>${teamMemberMarkup}<h4>
+        </div>
+        <div class="content">
+          <h6>ID:${engineer.id}</h6>
+          <h6>Email:${engineer.email}</h6>
+          <h6>GitHub:${engineer.gitHub}</h6>
+        </div>
+      </div>
+      <div class="card">
+      <div class="cardheader">
+      <h4>${teamMemberMarkup}</h4>
+      </div>
+      <div class="content">
+        <h6>ID:${engineer.id}</h6>
+        <h6>Email:${engineer.email}</h6>
+        <h6>GitHub:${engineer.gitHub}</h6>
+      </div>
+    </div>
+    <div class="card">
+    <div class="cardheader">
+    <h4>${teamMemberMarkup}</h4>
+    </div>
+    <div class="content">
+      <h6>ID:${engineer.id}</h6>
+      <h6>Email:${engineer.email}</h6>
+      <h6>GitHub:${engineer.gitHub}</h6>
+    </div>
+  </div>
+      <div class="card">
+        <div class="cardheader">
+        <h4>${teamMemberMarkup}<h4>
+        </div>
+        <div class="content">
+          <h6>ID:${intern.id}</h6>
+          <h6>Email:${intern.email}</h6>
+          <h6>School:${intern.school}</h6>
+        </div>
+      </div>
+      
+     
+
+
+
     ${teamMemberMarkup}
 `;
    
